6136d90bf410a9b6d75b4c1ef051369e16bf5e36
HoomanNateghian <hnateghian@outlook.com>
2024-10-09 20:42:18 +0330

    adding button
    I just added a button to calculate sum of numbers

---
+++ WindowsCalc/Calculations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsCalc
+{
+    internal class Calculations
+    {
+        public int sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
